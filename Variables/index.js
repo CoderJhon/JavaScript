61994e1a018b40b98bdcdbfe2cc83077b8321c64
Jhon <136621282+CoderJhon@users.noreply.github.com>
2024-06-09 21:05:26 -0400

    Add files via upload

---
+++ Variables/index.js
@@ -0,0 +1,47 @@
+/*  Variable = a container that stores a value.
+                behaves as if it were the value it contains
+
+    1. Declaration  let x ;
+    2. Assignment x = 100;
+        
+
+
+let age = 25;
+let price = 10.99;
+let gpa = 4;
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`You gpa is ${gpa} `);
+
+
+
+let firstName = "Jhon";
+let favoriteFood = "pizza";
+let email = "johnpalomino7911@gmail.com"
+
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`You like ${favoriteFood}`);
+console.log(`Your email is ${email}`);
+
+
+let online = false;
+let forSale = true;
+let isStudent = true;
+
+console.log(typeof online);
+console.log(`Jhon is online ${online}`);
+console.log(`Is this car for sale ${forSale}`);
+console.log(`Enrolled: ${isStudent}`);
+*/
+
+let fullName = "Coder Jhon"
+let age = 22;
+let student = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age}`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
+
+
